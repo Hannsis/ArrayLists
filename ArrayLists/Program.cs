70c56d72759b5785aeb3a997daa135cdef6b8a70
hannsis <hannamymadsen89@gmail.com>
2022-09-12 10:33:30 +0200

    Add project files.

---
+++ ArrayLists/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Arrays__lists__for_and_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays are used to store multiple values in a single variable,
+            //instead of declaring separate variables for each value.
+            Array();
+
+        }
+
+        static void Array()
+        {
+            string [] cuteAnimals = new string[3];
+            cuteAnimals[0] = "kitten";
+            cuteAnimals[1] = "cow";
+            cuteAnimals[2] = "puppy";
+            Console.WriteLine($"Oh look at that adorable {cuteAnimals[1]}");
+
+            string[] uncuteAnimals = { "spider", "cockroach", "marulk","rollie pollie" };
+            Console.WriteLine($"ett äckligt djur är: {uncuteAnimals[2]}");
+
+            string[] sötDjur = new string[3];
+            Console.WriteLine("Skriv in tre djur som du tycker är söta");
+            sötDjur[0] = Console.ReadLine();
+            sötDjur[1] = Console.ReadLine();
+            sötDjur[2] = Console.ReadLine();
+            Console.WriteLine($"tre sjur du gillar är: {sötDjur[2]}, {sötDjur[1]} och {sötDjur[0]}");
+        }
+    }
+
+}
